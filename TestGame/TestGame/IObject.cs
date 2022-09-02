3a686543cbd61255c0794af638fe903d6777bca9
AlenaShokina2412 <112576495+AlenaShokina2412@users.noreply.github.com>
2022-09-02 07:22:06 +0300

    Create IObject.cs

---
+++ TestGame/TestGame/IObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGame
+{
+    interface IObject   // интерфейс обекта 
+    {
+        public int Armor { get; set; }          //«Броня»
+        public double Life { get; set; }           //«Жизнь»
+        public double Health { get; set; }         //«Здоровье, которое в течение изменяется»
+        public int Damage { get; set; }         //«Урон»
+        public int CountBullet { get; set; }    //«Кол-во патронов»
+        
+        public double Shot( int EnemyArmor, int Probability);   //урон
+        public void Repair();           //починка
+        public void BuyBullet(int N);   // покупка пуль
+    }
+}
