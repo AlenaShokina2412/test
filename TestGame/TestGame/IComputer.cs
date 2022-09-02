4153607dfd61de67eb0a29bbacee52a7090f0ab8
AlenaShokina2412 <112576495+AlenaShokina2412@users.noreply.github.com>
2022-09-02 07:22:03 +0300

    Create IComputer.cs

---
+++ TestGame/TestGame/IComputer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGame
+{
+    interface IComputer// интерфейс компьютера
+    {
+        double ComputerStep( double Health, int Armor);
+    }
+}
