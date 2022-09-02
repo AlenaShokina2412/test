ab0f982285677019088e0335cb812ac90d881c8a
AlenaShokina2412 <112576495+AlenaShokina2412@users.noreply.github.com>
2022-09-02 07:23:14 +0300

    Create Program.cs

---
+++ TestGame/TestGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestGame
+{ 
+    class Program
+    {
+        /*жизнь, урон и броня компьютера*/
+        const double LifeTankPlayer = 30;
+        const int ArmorTankPlayer = 4;
+        const int DamageTankPlayer = 6;
+
+        /*жизнь, урон и броня игрока*/
+        const double LifeTankComputer = 20;
+        const int ArmorTankComputer = 3;
+        const int DamageTankComputer = 7;
+
+        static void Main(string[] args)
+        {
+            /*Создание объекта и запуск игры*/
+            Object Gme = new Object(ArmorTankPlayer,LifeTankPlayer,DamageTankPlayer, 5 , ArmorTankComputer, LifeTankComputer,  DamageTankComputer,5);
+            Gme.AlgorithmGame();
+        }
+    }
+}
